e63cab56774c3ece468a0a81e3ffdc6b627f690c
Arif <arif@pragmakw.com>
2022-11-15 22:02:58 +0530

    added require rails

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, "git@github.com:itzworkstation/challenge_me.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-
+set :branch, 'aws-eb'
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, "/home/deploy/challenge_me_dev"
 
